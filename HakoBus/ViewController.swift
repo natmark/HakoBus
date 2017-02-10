0bc1f3fc9a4ec622ca2433d2dcfebf4cd8dd1605
natmark <natmark0918@gmail.com>
2017-02-10 09:07:35 +0900

    :construction: add xcode proj

---
+++ HakoBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HakoBus
+//
+//  Created by AtsuyaSato on 2017/02/10.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
