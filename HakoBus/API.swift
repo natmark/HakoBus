ace62dfd4e30ba24d570724c1a1aec606d5e1664
natmark <natmark0918@gmail.com>
2017-02-12 22:13:10 +0900

    :sparkles: add function of meintenance confirmer

---
+++ HakoBus/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  HakoBus
+//
+//  Created by AtsuyaSato on 2017/02/12.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class API {
+    //managerを定義
+    static let manager = Alamofire.SessionManager(configuration: URLSessionConfiguration.default)
+ }
--- HakoBus/ViewController.swift
+++ HakoBus/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
-
+import RxSwift
 class ViewController: UIViewController {
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        API.Location.isMeintenance().subscribe(onNext: { isMeintenance in
+            print (isMeintenance)
+        }, onError: nil)
+        .addDisposableTo(disposeBag)
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
