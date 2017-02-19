46f2b49f93deac0b05332289dcd45c8c10930f0e
natmark <natmark0918@gmail.com>
2017-02-19 15:53:02 +0900

    :sparkles: add route search function

--- HakoBus/Route.swift
+++ HakoBus/Route.swift
@@ -10,6 +10,8 @@ import Foundation
 import RxSwift
 import RxAlamofire
 import Alamofire
+import ObjectMapper
+import Kanna
 
 extension Router {
     enum Route: URLRequestConvertible {
@@ -107,8 +109,37 @@ extension API {
                         .validate(statusCode: 200 ..< 300)
                         .rx.responseString(encoding: String.Encoding.shiftJIS)
                         .flatMap { (res,html) -> Observable<RouteSearchResultParameters> in
+                            let doc = HTML(html: html, encoding: String.Encoding.utf8)
                            //Kannaで抜き出して return
-                            let results = [RouteSearchResultParameters]()
+                            var results = [RouteSearchResultParameters]()
+                            
+                            let result = doc?.css("div#result").first
+                            for (index,table_row) in (result?.css("tr"))!.enumerated() {
+                                if index == 0 { continue } //最初の要素はヘッダ行
+                                let table_data = table_row.css("td")
+                                
+                                var routeSearchResultParameters = Mapper<RouteSearchResultParameters>().map(JSONString: "{}")
+                                
+                                //停留所時刻
+                                routeSearchResultParameters?.departure = (table_data[1].css("div").first?.innerHTML)!
+                                if let boarding = table_data[3].css("div").first?.css("a").first {
+                                    //乗り場
+                                    routeSearchResultParameters?.boarding_place = boarding.innerHTML!
+                                    //マップURL
+                                    routeSearchResultParameters?.map_url = boarding["href"]!
+                                }
+                                //行き先
+                                routeSearchResultParameters?.destination = (table_data[4].css("div").first?.innerHTML)!
+                                
+                                //停留所時刻のURL
+                                routeSearchResultParameters?.schedule_url = ((table_data[5].css("div").first?.css("a").first)?["href"]!)!
+                                
+                                //運行状況
+                                routeSearchResultParameters?.operation_status = table_data[7].innerHTML!
+                                
+                                results.append(routeSearchResultParameters!)
+                            }
+                            
                             return Observable.from(results)
                     }
                 }
